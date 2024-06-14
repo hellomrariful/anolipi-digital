d1cf813e3e75f6a3561316e14f331881d461abd8
Ariful Islam <hello.mrariful@gmail.com>
2024-06-14 14:24:46 +0600

    footer section updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
-
 import Link from "next/link";
-import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaFacebook, FaLinkedin, FaMailBulk } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
 
 const Footer = () => {
   return (
@@ -9,20 +9,17 @@ const Footer = () => {
         <footer className="bg-gradient-to-br from-[#a600fe] to-[#a600fe]">
           <div className="container py-10 mx-auto">
             <div className="flex flex-col items-center text-center">
-              <Link href="/"  className="flex items-center">
+              <Link href="/" className="flex items-center">
                 {/* <img src={logo} className="h-12 mr-3" alt="" /> */}
-                <span className="text-3xl font-bold">
-                  Anolipi Digital LLC
-                </span>
+                <span className="text-3xl font-bold">Anolipi Digital LLC</span>
               </Link>
 
               <div className="flex flex-wrap justify-center mt-6  text-darkGray">
-               
                 <a
                   href=""
                   className="mx-4 transition-colors duration-300 hover:text-[#524FF5]"
                 >
-              About
+                  About
                 </a>
                 <a
                   href=""
@@ -36,7 +33,7 @@ const Footer = () => {
                 >
                   Terms & Condition
                 </a>
-                
+
                 <a
                   href=""
                   className="mx-4 transition-colors duration-300 hover:text-[#524FF5]"
@@ -47,7 +44,7 @@ const Footer = () => {
                   href=""
                   className="mx-4  transition-colors duration-300 hover:text-[#524FF5]"
                 >
-                  Return Policy
+                  Refund Policy
                 </a>
                 <a
                   href=""
@@ -55,8 +52,20 @@ const Footer = () => {
                 >
                   Book a Demo
                 </a>
-                
               </div>
+            </div>
+
+            <div className="grid justify-center item-center mx-auto mt-2 text-darkGray">
+              <h3 className="mx-auto">
+                Mail: digital@anolipi.com
+              </h3>
+              <h3 className="mx-auto">
+                Contact: +1 (505) 509-0709
+              </h3>
+              <h3 className="mx-auto">
+                Location: 123 Street, Albuquerque, USA
+              </h3>
+
             </div>
 
             <div className="px-6 -mb-2">
@@ -67,13 +76,13 @@ const Footer = () => {
                   © Anolipi Digital LLC 2024. All Rights Reserved.
                 </p>
 
-                <div className="flex  -mx-2 md:mt-0 mt-3">
+                <div className="flex -mx-2 md:mt-0 mt-3 justify-center item-center ">
                   <a
-                    href="https://github.com/hellomrariful"
-                    className="mx-2 text-xl transition-colors duration-300 dark:text-gray-300 hover:text-[#524FF5] dark:hover:text-blue-400"
+                    href="https://anolipi.com/"
+                    className="mx-2 text-[27px] -mt-1 transition-colors duration-300 dark:text-gray-300 hover:text-[#524FF5] dark:hover:text-blue-400"
                     aria-label="GitHub"
                   >
-                    <FaGithub></FaGithub>
+                    <IoIosMail />
                   </a>
                   <a
                     href="https://www.facebook.com/Anolipi.digital"
