30b430de0c5b1951029fa210e8fcecbc80ba8079
Ariful Islam <hello.mrariful@gmail.com>
2024-11-10 13:20:52 +0600

    adjust map lg screen

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -4,7 +4,7 @@ import "./Map.css";
 
 const Map = () => {
   return (
-    <div className="map max-w-7xl mx-5 md:mx-10  3xl:mx-auto h-[140px] xs:h-40 sm:h-[220px] md:h-[270px] lg:h-[360px] my-10 2xl:my-20"></div>
+    <div className="map max-w-7xl 3xl:mx-auto h-[140px] xs:h-40 sm:h-[220px] md:h-[270px] lg:h-[460px] my-10 2xl:my-20"></div>
   );
 };
 
